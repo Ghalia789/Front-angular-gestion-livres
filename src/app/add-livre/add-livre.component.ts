3633c44353839ede788c764c573aaf13d34c8706
Ghalia789 <ghaliabellalouna@gmail.com>
2023-05-17 22:24:56 +0100

    modified:   src/app/add-livre/add-livre.component.html 	modified:   src/app/add-livre/add-livre.component.ts 	modified:   src/app/livres/livres.component.ts 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/livres/livres.component.html 	modified:   src/app/model/livre.model.ts 	modified:   src/app/service/livre.service.ts 	modified:   src/app/update-livre/update-livre.component.html 	modified:   src/app/update-livre/update-livre.component.ts 	src/app/model/genre.model.ts

--- src/app/add-livre/add-livre.component.ts
+++ src/app/add-livre/add-livre.component.ts
@@ -1,6 +1,8 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Livre } from '../model/livre.model';
 import { LivreService } from '../service/livre.service';
+import { Genre } from '../model/genre.model';
 @Component({
   selector: 'app-add-livre',
   templateUrl: './add-livre.component.html',
@@ -9,14 +11,23 @@ import { LivreService } from '../service/livre.service';
 export class AddLivreComponent implements OnInit {
 
   newLivre = new Livre();
-  constructor(private livreService: LivreService) {
-
+  genres! : Genre[];
+  newIdGen!: number;
+  newGenre! : Genre;
+  constructor(private livreService: LivreService,
+    private router :Router) {
+    this.genres = this.livreService.listeGenres();
   }
 
   ngOnInit(): void {
 
   }
   addLivre() {
+    this.newGenre = this.livreService.consulterGenre(this.newIdGen);
+    this.newLivre.genre = this.newGenre;
     this.livreService.ajouterLivre(this.newLivre);
+    this.router.navigate(['livres']);
   }
+
+
 }
--- src/app/livres/livres.component.ts
+++ src/app/livres/livres.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Livre } from '../model/livre.model';
 import { LivreService } from '../service/livre.service';
+import { Genre } from '../model/genre.model';
 @Component({
   selector: 'app-livres',
   templateUrl: './livres.component.html',
