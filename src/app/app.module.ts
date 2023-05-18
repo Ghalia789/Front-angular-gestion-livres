95e3ee25990bf2601041911e1d1fa7470e982f88
Ghalia789 <ghaliabellalouna@gmail.com>
2023-05-18 11:56:42 +0100

    modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {HttpClientModule } from '@angular/common/http';
 import { RechercheParGenreComponent } from './recherche-par-genre/recherche-par-genre.component';
 import { RechercheParTitreComponent } from './recherche-par-titre/recherche-par-titre.component';
 import { SearchFilterPipe } from './search-filter.pipe';
+//import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +26,8 @@ import { SearchFilterPipe } from './search-filter.pipe';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    //Ng2SearchPipeModule
   ],
   providers: [LivreService],
   bootstrap: [AppComponent]
